6a8e1dd8b52b254cf8830d73af3bff87cd7861dd
dola <sanjogitabhagowati11@gmail.com>
2025-05-14 09:00:37 +0530

    Create 5rottingOranges.cpp

---
+++ GRAPHS1/5rottingOranges.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) 
+    {
+        if (grid.empty() || grid[0].empty()) return -1;
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int,int>> q;
+        int fresh = 0;
+
+        for (int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if (grid[i][j]==2)
+                {
+                    q.push({i,j});
+                }
+                else if (grid[i][j]==1)
+                {
+                    fresh ++;
+                }
+            }
+        }
+
+        if (fresh == 0) return 0;
+
+        int minutes = 0;
+        vector<pair<int,int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.empty())
+        {
+            int sz = q.size();
+            bool rotten = false;
+            for (int i=0; i<sz; i++)
+            {
+                auto[x,y] = q.front();
+                q.pop();
+                for (auto [dx,dy] : directions)
+                {
+                    int nx = x + dx;
+                    int ny = y + dy;
+                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 1)
+                    {
+                        grid[nx][ny] = 2;
+                        q.push({nx,ny});
+                        fresh --;
+                        rotten = true;
+                    }
+                }
+            }
+            if (rotten) minutes++;
+        }
+        return fresh == 0 ? minutes : -1;
+    }
+};
+
+
+//SC : O(m*n)
+//TC : O(m*n)
