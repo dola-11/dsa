c635f97cc867c146fd7a2e8e1fd4fd34c5da2294
dola <sanjogitabhagowati11@gmail.com>
2025-05-16 14:56:00 +0530

    Create 8zeroOneMatrix.cpp

---
+++ GRAPHS1/8zeroOneMatrix.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
+    {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<vector<int>>distMatrix(m,vector<int>(n,0));
+        vector<vector<int>>visited(m,vector<int>(n,0));
+
+        queue<pair<pair<int,int>,int>> q;
+
+        for(int i=0; i<m;i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(mat[i][j] == 0)
+                {
+                    q.push({{i,j},0});
+                    visited[i][j] = 1;
+                }
+            }
+        }
+
+        while(!q.empty())
+        {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int dist = q.front().second;
+
+            q.pop();
+
+            distMatrix[row][col] = dist;
+
+            vector<pair<int,int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
+            for (auto [dx,dy] : directions)
+            {
+                int nrow = row + dx;
+                int ncol = col + dy;
+                if (nrow >= 0 && ncol >= 0 && nrow < m && ncol < n && mat[nrow][ncol] == 1)
+                {
+                    mat[nrow][ncol] = 0;
+                    q.push({{nrow,ncol},dist+1});
+
+                }
+            }
+        }
+        return distMatrix;
+    }
+};
+
+//SC : O(m*n)
+//TC : O(m*n)
