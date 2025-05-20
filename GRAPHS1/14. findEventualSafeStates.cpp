bec86f0a3358b25653ca2048dc612f9ff0b0f73c
dola <sanjogitabhagowati11@gmail.com>
2025-05-20 11:06:35 +0530

    Update 14. findEventualSafeStates.cpp

--- GRAPHS1/14. findEventualSafeStates.cpp
+++ GRAPHS1/14. findEventualSafeStates.cpp
@@ -94,3 +94,53 @@ public:
         return safeNodes;
     }
 };
+
+//using kahn's
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
+    {
+        int n = graph.size();
+        vector<vector<int>> revGraph(n);
+        vector<int> inDegree(n, 0);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (auto it : graph[i])
+            {
+                revGraph[it].push_back(i);   // Reversed edge
+                inDegree[i]++;              // Original outdegree -> now inDegree
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+        {
+            if (inDegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+
+        vector<int> safeNodes;
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            safeNodes.push_back(node);
+
+            for (auto it : revGraph[node])
+            {
+                inDegree[it]--;
+                if (inDegree[it] == 0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+
+        sort(safeNodes.begin(), safeNodes.end());
+        return safeNodes;
+    }
+};
