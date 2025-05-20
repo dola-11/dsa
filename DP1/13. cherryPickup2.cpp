e54db43d8c4a97591892ec99616facc0499560e1
dola <sanjogitabhagowati11@gmail.com>
2025-05-20 13:03:31 +0530

    Create 13. cherryPickup2.cpp

---
+++ DP1/13. cherryPickup2.cpp
@@ -0,0 +1,172 @@
+//3d DP problem
+
+//Memoization
+class Solution {
+public:
+    int memoization(int i, int j1, int j2, vector<vector<int>> &grid, vector<vector<vector<int>>> &dp)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        if(j1<0 || j1>=m || j2<0 || j2>=m)
+        {
+            return -1e9;
+        }
+
+        if (i == n-1)
+        {
+            if (j1 == j2) return grid[i][j1];
+            else return grid[i][j1] + grid[i][j2];
+        }
+
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+
+        //explore all paths of robot1 and robot2 simultaneously
+        int maxi = -1e9;
+        for (int dj1 = -1 ; dj1 <= 1; dj1++)
+        {
+            for (int dj2 = -1; dj2 <= 1; dj2++)
+            {
+                int value = 0;
+                if (j1 == j2) value = grid[i][j1];
+                else value = grid[i][j1] + grid[i][j2];
+
+                value += memoization(i+1, j1+dj1, j2+dj2,grid,dp);
+                maxi = max (maxi,value);
+            }
+        }
+        return dp[i][j1][j2] = maxi;
+    }
+
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
+
+        return memoization(0,0,m-1,grid,dp);
+    }
+};
+
+//TC : O(n⋅m⋅m⋅3⋅3) = O(n.n.m)
+//SC : O(n.m^2) + O(n)
+
+//Tabulation
+class Solution {
+public:
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+
+        //base cases
+        for (int j1 = 0; j1<m; j1++)
+        {
+            for (int j2 = 0; j2<m; j2++)
+            {
+                if (j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+
+        //nested loops
+        for (int i = n-2; i>=0; i--)
+        {
+            for (int j1= 0; j1<m; j1++)
+            {
+                for (int j2 = 0; j2<m; j2++)
+                {
+                    int maxi = -1e9;
+                    for (int dj1 = -1 ; dj1 <= 1; dj1++)
+                    {
+                        for (int dj2 = -1; dj2 <= 1; dj2++)
+                        {
+                            int value = 0;
+                            if (j1 == j2) value = grid[i][j1];
+                            else value = grid[i][j1] + grid[i][j2];
+
+                            if (j1+dj1 >= 0 && j1+dj1 < m && j2+dj2 >= 0 && j2+dj2 < m)
+                            {
+                                value += dp[i+1][j1+dj1][j2+dj2];
+                            }
+                            else
+                            {
+                                value = -1e9;
+                            }
+                            maxi = max (maxi,value);
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+    }
+};
+
+//TC : O(n⋅m⋅m⋅3⋅3) = O(n.n.m)
+//SC : O(n.m^2)
+
+//Space optimization
+class Solution {
+public:
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>>front(m,vector<int>(m,0));
+        vector<vector<int>>curr(m,vector<int>(m,0));
+
+
+        //base cases
+        for (int j1 = 0; j1<m; j1++)
+        {
+            for (int j2 = 0; j2<m; j2++)
+            {
+                if (j1 == j2) front[j1][j2] = grid[n-1][j1];
+                else front[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+
+        //nested loops
+        for (int i = n-2; i>=0; i--)
+        {
+            for (int j1= 0; j1<m; j1++)
+            {
+                for (int j2 = 0; j2<m; j2++)
+                {
+                    int maxi = -1e9;
+                    for (int dj1 = -1 ; dj1 <= 1; dj1++)
+                    {
+                        for (int dj2 = -1; dj2 <= 1; dj2++)
+                        {
+                            int value = 0;
+                            if (j1 == j2) value = grid[i][j1];
+                            else value = grid[i][j1] + grid[i][j2];
+
+                            if (j1+dj1 >= 0 && j1+dj1 < m && j2+dj2 >= 0 && j2+dj2 < m)
+                            {
+                                value += front[j1+dj1][j2+dj2];
+                            }
+                            else
+                            {
+                                value = -1e9;
+                            }
+                            maxi = max (maxi,value);
+                        }
+                    }
+                    curr[j1][j2] = maxi;
+                }
+            }
+            front = curr;
+        }
+        return front[0][m-1];
+    }
+};
+
+//TC : O(n⋅m⋅m⋅3⋅3) = O(n.n.m)
+//SC : O(m^2)
