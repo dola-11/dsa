7b441c1cc85a09fdcbd0182d3088d7a2ecf935fb
dola <sanjogitabhagowati11@gmail.com>
2025-05-20 07:28:56 +0530

    Update 3.noOfProvinces.cpp

--- GRAPHS1/3.noOfProvinces.cpp
+++ GRAPHS1/3.noOfProvinces.cpp
@@ -26,3 +26,50 @@ public:
         return numberOfComponents;
     }
 };
+
+//tried converting the matrix to list
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>> &adjList, vector<bool> &visit)
+    {
+        visit[node] = true;
+
+        for (auto it : adjList[node])
+        {
+            if(!visit[it])
+            {
+                dfs(it,adjList,visit);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) 
+    {
+        int n = isConnected.size();
+        vector<vector<int>>adjList(n);
+
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<n; j++)
+            {
+                if (isConnected[i][j] && i!=j)
+                {
+                    adjList[i].push_back(j);
+                }
+            }
+        }
+
+        vector<bool>visit(n,false);
+        int noOfComponents = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(!visit[i])
+            {
+                noOfComponents++;
+                dfs(i,adjList,visit);
+            }
+        }
+        return noOfComponents;
+    }
+};
