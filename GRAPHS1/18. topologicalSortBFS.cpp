9530bb60acbce5b07bc5817e807aee404edc9092
dola <sanjogitabhagowati11@gmail.com>
2025-05-20 09:31:29 +0530

    Create 18. topologicalSortBFS.cpp

---
+++ GRAPHS1/18. topologicalSortBFS.cpp
@@ -0,0 +1,49 @@
+//This is also known as Kahn's algorithm
+class Solution{
+public:
+    vector<int> topoSort(int V, vector<int> adj[])
+    {
+        vector<int>inDegree(V,0);
+
+        for (int i=0; i<V; i++)
+        {
+            for (auto it : adj[i])
+            {
+                inDegree[it] ++;
+            }
+        }
+
+        queue<int> q;
+
+        for (int i=0; i<V; i++)
+        {
+            if (inDegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+
+        vector<int>topo;
+
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for (auto it : adj[node])
+            {
+                inDegree[it]--;
+                if (inDegree[it] == 0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+
+        return topo;
+    }
+};
+
+//TC : O(V) + O(V) + O(V+E) = O(V+E)
+//SC : O(V) + O(V) + O(V) + O(V+E) = O(V+E)
