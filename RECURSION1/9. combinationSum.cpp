ee1a9d03050b56488c54ba6b5a0982226b33b010
dola <sanjogitabhagowati11@gmail.com>
2025-05-23 22:30:34 +0530

    Create 9. combinationSum.cpp

---
+++ RECURSION1/9. combinationSum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void sumCombinations(int index, int sum, int target, int n, vector<int> &candidates, vector<int> &sol, vector<vector<int>> &result)
+    {
+        if (sum == target )
+        {
+            result.push_back(sol);
+            return;
+        }
+
+        if (sum > target || index >= n)
+        {
+            return;
+        }
+        
+        //considering the next
+        sumCombinations(index+1,sum,target,n,candidates,sol,result);
+
+        //considering the current any no of times
+        sol.push_back(candidates[index]);
+
+        //sum is passed by value, not by reference.
+        sumCombinations(index,sum+candidates[index],target,n,candidates,sol,result);
+        sol.pop_back();
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+    {
+        int n = candidates.size();
+        vector<vector<int>>result;
+        vector<int>sol;
+        sumCombinations(0,0,target,n, candidates,sol,result);
+        return result;
+    }
+};
+
+// TC: O(2^n * target),  2^n from considering each element either included or excluded, T because of repeated picking of the same element to reach the target.
+// SC: exponential in the worst case.
