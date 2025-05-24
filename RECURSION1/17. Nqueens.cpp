2c7df78331fb038e6fd114dbd6537dee674b7162
dola <sanjogitabhagowati11@gmail.com>
2025-05-24 17:15:40 +0530

    Create 17. Nqueens.cpp

---
+++ RECURSION1/17. Nqueens.cpp
@@ -0,0 +1,126 @@
+//Method 1
+
+class Solution {
+public:
+    bool isSafe(int row, int col, vector<string> &board, int n)
+    {
+        int duprow = row;
+        int dupcol = col;
+
+        while(row>=0 && col >= 0)
+        {
+            if (board[row][col] == 'Q') return false;
+
+            col--;
+            row--;
+        }
+
+        row = duprow;
+        col = dupcol;
+
+        while(col >= 0)
+        {
+            if (board[row][col] == 'Q') return false;
+            
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+
+        while(row<n && col>=0)
+        {
+            if (board[row][col] == 'Q') return false;
+
+            row++;
+            col--;
+        }
+        return true;
+    }
+    void solve(int col, vector<string> &board, vector<vector<string>> &solution, int n)
+    {
+        if (col == n)
+        {
+            solution.push_back(board);
+            return;
+        }
+
+        for (int row =0; row<n; row++)
+        {
+            if (isSafe(row,col,board,n))
+            {
+                board[row][col] = 'Q';
+                solve(col+1,board,solution,n);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) 
+    {
+        vector<vector<string>> solution;
+        vector<string> board(n);
+        string s(n,'.');
+        for (int i=0; i<n; i++)
+        {
+            board[i] = s;
+        }
+
+        solve(0,board,solution,n);
+        return solution;
+    }
+};
+
+//TC : O(N! * N)
+//SC : O(N! * N + N^2)
+
+//Method 2:
+//Using hashing
+
+class Solution {
+public:
+    void solve(int col, vector<vector<string>> &ans, int n, vector<string> & board, vector<int>leftRow, vector<int>upperDiagonal, vector<int>lowerDiagonal)
+    {
+        if (col == n)
+        {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int row = 0; row <n; row++)
+        {
+            if (leftRow[row] == 0 && upperDiagonal[row+col] == 0 && lowerDiagonal[n-1 + col-row] == 0)
+            {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                upperDiagonal[row+col] = 1;
+                lowerDiagonal[n-1 + col - row] = 1;
+                solve(col+1,ans,n,board,leftRow,upperDiagonal,lowerDiagonal);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                upperDiagonal[row+col] = 0;
+                lowerDiagonal[n-1 + col - row] = 0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) 
+    {
+        vector<vector<string>> ans;
+        vector<string>board(n);
+        string s(n, '.');
+        for (int i=0;i<n;i++)
+        {
+            board[i] = s;
+        }
+
+        vector<int>leftRow(n,0);
+        vector<int>upperDiagonal(2*n-1,0);
+        vector<int>lowerDiagonal(2*n-1,0);
+
+        solve(0,ans,n,board,leftRow,upperDiagonal,lowerDiagonal);
+        return ans;
+    }
+};
+
+//TC : O(N! * N)
+//SC : O(N! * N + N^2)
