b728e801e6efb4e451d9753bdb1d0800d1a01350
dola <sanjogitabhagowati11@gmail.com>
2025-06-02 11:30:30 +0530

    Update 1. atoi.cpp

--- RECURSION1/1. atoi.cpp
+++ RECURSION1/1. atoi.cpp
@@ -3,12 +3,12 @@ class Solution {
 public:
     int myAtoi(string s) 
     {
-        int i=0;
+        int i = 0;
         int n = s.length();
-        while(i<n && s[i]== ' ')i++;
+        while(i<n && s[i] == ' ') i++;
 
         int sign = 1;
-        if(i<n && (s[i] == '+' || s[i]== '-'))
+        if (i<n && (s[i] == '+' || s[i] == '-'))
         {
             sign = (s[i] == '-') ? -1 : 1;
             i++;
@@ -17,17 +17,20 @@ public:
         long result = 0;
         while(i<n && isdigit(s[i]))
         {
-            result = result*10 + (s[i]-'0');
+            int digit = s[i] - '0';
 
-            if (sign * result >= INT_MAX) return INT_MAX;
-            if (sign * result <= INT_MIN) return INT_MIN;
+            if (result > (INT_MAX - digit) / 10)
+            {
+                return (sign == 1) ? INT_MAX : INT_MIN;   
+            }
+            
+            result = result*10 + digit;
 
             i++;
         }
-        return sign * result;
+        return sign* result;
     }
 };
-
 //TC : O(n)
 //SC : O(1)
 
