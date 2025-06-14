8c7869ff2e95a02c5d377377b6144c5016ca8e6a
dola <sanjogitabhagowati11@gmail.com>
2025-06-14 08:20:02 +0530

    Create 11. Missing Number.cpp

---
+++ ARRAYS1/11. Missing Number.cpp
@@ -0,0 +1,39 @@
+//Method 1:
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int sum = n*(n+1)/2;
+        int reqSum = 0;
+        for (int i=0; i<n; i++)
+        {
+            reqSum += nums[i];
+        }
+        return sum - reqSum;
+    }
+};
+
+//Method 2:
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        //2nd solution using XOR
+        int n = nums.size();
+        int xorSum = 0;
+
+        for (int i=0; i<= n; i++)
+        {
+            xorSum ^= i;
+        }
+
+        for (int num: nums)
+        {
+            xorSum ^= num;
+        }
+
+        return xorSum;
+    }
+};
