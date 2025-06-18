4a19512468d3e3f66335c9c4b2b83a1d69553c31
dola <sanjogitabhagowati11@gmail.com>
2025-06-18 12:36:50 +0530

    Create 28. Update to children sum property.cpp

---
+++ TREES1/28. Update to children sum property.cpp
@@ -0,0 +1,72 @@
+// TreeNode structure
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+
+
+class Solution {
+public:
+    // Function to change the values of the nodes
+    // based on the sum of its children's values.
+    void changeTree(TreeNode* root) {
+        // Base case: If the current node
+        // is NULL, return and do nothing.
+        if (root == NULL) {
+            return;
+        }
+
+        // Calculate the sum of the values of
+        // the left and right children, if they exist.
+        int child = 0;
+        if (root->left) {
+            child += root->left->val;
+        }
+        if (root->right) {
+            child += root->right->val;
+        }
+
+        // Compare the sum of children with
+        // the current node's value and update
+        if (child >= root->val) {
+            root->val = child;
+        } else {
+            // If the sum is smaller, update the
+            // child with the current node's value.
+            if (root->left) {
+                root->left->val = root->val;
+            } else if (root->right) {
+                root->right->val = root->val;
+            }
+        }
+
+        // Recursively call the function
+        // on the left and right children.
+        changeTree(root->left);
+        changeTree(root->right);
+
+        // Calculate the total sum of the
+        // values of the left and right
+        // children, if they exist.
+        int tot = 0;
+        if (root->left) {
+            tot += root->left->val;
+        }
+        if (root->right) {
+            tot += root->right->val;
+        }
+
+        // If either left or right child
+        // exists, update the current node's
+        // value with the total sum.
+        if (root->left or root->right) {
+            root->val = tot;
+        }
+    }
+};
+
+//Time Complexity: O(N) -- Every node is visited once.
+//Space Complexity: O(H) --  Due to recursive stack (H is the height of the tree)
