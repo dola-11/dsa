3355808fa66afb00ed913ba29bcd028bc275c2be
dola <sanjogitabhagowati11@gmail.com>
2025-06-18 17:58:02 +0530

    Create 35. Morris Inorder traversal.cpp

---
+++ TREES1/35. Morris Inorder traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution {
+public:
+    vector<int> getInorder(TreeNode* root) 
+    {
+    	//your code goes here
+        vector<int>inorder;
+        TreeNode* current = root;
+
+        while(current != NULL)
+        {
+            if (current -> left == NULL)
+            {
+                inorder.push_back(current -> val);
+                current = current -> right;
+            }
+            else
+            {
+                TreeNode* prev = current -> left;
+                while(prev -> right && prev -> right != current)
+                {
+                    prev = prev -> right;
+                }
+
+                if (prev -> right == NULL)
+                {
+                    prev -> right == current;
+                    current = current -> left;
+                }
+                else
+                {
+                    prev->right == NULL;
+                    inorder.push_back(current -> val);
+                    current = current -> right;
+                }
+            }
+        }
+    }
+};
+
+//TC : O(N)
+//SC : O(1)
