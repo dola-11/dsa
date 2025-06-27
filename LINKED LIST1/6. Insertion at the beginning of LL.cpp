3b80e630ab34e4a173513bf2b626b1b70fe5e58d
dola <sanjogitabhagowati11@gmail.com>
2025-06-27 10:39:45 +0530

    Create 6. Insertion at the beginning of LL.cpp

---
+++ LINKED LIST1/6. Insertion at the beginning of LL.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    // Function to insert a node at the begining of the linked list.
+    Node *insertAtBegining(Node *head, int x)
+    {
+        // Code here
+        Node* temp = new Node(x);
+        temp -> next = head;
+        return temp;
+    }
+};
+
+//O(1) - SC,TC
