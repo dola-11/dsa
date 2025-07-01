8043f207a8f2067cb2850535fed1bb09625bfa69
dola <sanjogitabhagowati11@gmail.com>
2025-07-02 00:38:26 +0530

    Create 3. Find the Original Typed String I.cpp

---
+++ STRINGS1/3. Find the Original Typed String I.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int possibleStringCount(string word) 
+    {
+        int count = 1;
+        for (int i=0; i<word.size(); i++)
+        {
+            if(word[i] == word[i+1])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+//TC : O(N)
+//Sc : O(1)
