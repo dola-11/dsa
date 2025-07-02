cf7eec5fb0f91b32060cbf7133e17fef0290a983
dola <sanjogitabhagowati11@gmail.com>
2025-07-02 10:54:54 +0530

    Create 8. Rotate String.cpp

---
+++ STRINGS1/8. Rotate String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) 
+    {
+        if(s.length() != goal.length())
+        {
+            return false;
+        }
+
+        string doubled = s + s;
+        return doubled.find(goal) != string::npos;
+    }
+};
+
+//SC : O(n)
+//TC : O(N)
