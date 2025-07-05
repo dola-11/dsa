d0f65fa37b288ccc02f60cf9bf9d8659a69dd295
dola <sanjogitabhagowati11@gmail.com>
2025-07-05 11:28:41 +0530

    Create 8. Binary Subarrays With Sum.cpp

---
+++ TWO POINTERS AND SLIDING WINDOWS1/8. Binary Subarrays With Sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countSubarray(vector<int> &nums, int goal)
+    {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int count = 0;
+
+        if (goal < 0) return 0;
+
+        while(right < nums.size())
+        {
+            sum += nums[right];
+            while(sum > goal)
+            {
+                sum -= nums[left];
+                left++;
+            }
+            count += right-left+1;
+            right++;
+        }
+        return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) 
+    {
+        return countSubarray(nums,goal) - countSubarray(nums, goal-1);
+    }   
+};
+
+//TC : O(N)
+//SC : O(1)
