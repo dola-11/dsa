3dad978bbc838a35a310618127d2082f0ba78848
dola <sanjogitabhagowati11@gmail.com>
2025-07-05 18:59:08 +0530

    Create 5. Jump Game 2.cpp

---
+++ GREEDY1/5. Jump Game 2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) 
+    {
+        int jumps = 0;
+        int right = 0;
+        int left = 0;
+        int n = nums.size();
+
+        while(right<n-1)
+        {
+            int farthest = 0;
+            for (int i=left; i<=right; i++)
+            {
+                farthest = max(farthest, i + nums[i]);
+            }
+            left = right + 1;
+            right = farthest;
+            jumps = jumps + 1;
+        }
+        return jumps;
+    }
+};
+
+//TC : O(N)
+//SC : O(1)
