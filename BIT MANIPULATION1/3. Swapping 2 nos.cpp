8adfb7caa59737932830bb1aef6e98492fffe3f7
dola <sanjogitabhagowati11@gmail.com>
2025-07-12 18:30:00 +0530

    Create 3. Swapping 2 nos.cpp

---
+++ BIT MANIPULATION1/3. Swapping 2 nos.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    pair<int, int> get(int a, int b) {
+        // code here
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        
+        return{a,b};
+    }
+};
+
+//TC : O(1)
+//SC : O(1)
