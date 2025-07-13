7c81958940037df5903a35711fc12779492e6ab5
dola <sanjogitabhagowati11@gmail.com>
2025-07-13 17:14:45 +0530

    Create 2. Reverse Integer.cpp

---
+++ BASIC AND ADV MATHS1/2. Reverse Integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverse(int x) 
+    {
+        int revnum = 0;
+        while(x!=0)
+        {
+            int ld = x%10;
+            if ((revnum > INT_MAX/10) || (revnum <INT_MIN/10))
+            {
+                return 0;
+            }
+            else
+            {
+                revnum = revnum*10 + ld;
+                x = x/10;
+            }
+        }
+        return revnum;
+    }
+};
+
+//TC : O(log₁₀ x)
+//SC : 	O(1)
