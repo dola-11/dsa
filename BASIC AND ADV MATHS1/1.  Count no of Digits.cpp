8f572a2467602e71cc0c8086d6d38e2e53169908
dola <sanjogitabhagowati11@gmail.com>
2025-07-13 17:03:45 +0530

    Update 1.  Count no of Digits.cpp

--- BASIC AND ADV MATHS1/1.  Count no of Digits.cpp
+++ BASIC AND ADV MATHS1/1.  Count no of Digits.cpp
@@ -15,3 +15,11 @@ class Solution {
 
 //TC : Time: O(log₁₀ n)
 //SC : O(1)
+
+int countDigits(int n) {
+    if (n == 0) return 1;  // Special case: log10(0) is undefined
+    return (int)(log10(n) + 1);
+}
+
+//TC : O(1)
+//SC : O(1)
