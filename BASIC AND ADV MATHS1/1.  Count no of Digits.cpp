e0e5b1394dba753663a1dd1c650cd98e35c0410e
dola <sanjogitabhagowati11@gmail.com>
2025-07-13 16:58:14 +0530

    Create 1.  Count no of Digits.cpp

---
+++ BASIC AND ADV MATHS1/1.  Count no of Digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int countDigits(int n) 
+    {
+        // code here
+        int cnt = 0;
+        while(n>0)
+        {
+            cnt++;
+            n = n/10;
+        }
+        return cnt;
+    }
+};
+
+//TC : Time: O(log₁₀ n)
+//SC : O(1)
