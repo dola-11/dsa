031a117526040a2c32f5575dd8b4723dfdd6ea74
dola <sanjogitabhagowati11@gmail.com>
2025-07-13 13:47:21 +0530

    Create 17. XOR Operation in an Array.cpp

---
+++ BIT MANIPULATION1/17. XOR Operation in an Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) 
+    {
+        int xorr = 0;
+        for (int i = 0; i < n; i++)
+        {
+            xorr ^= start + 2 * i;
+        }
+        return xorr;
+    }
+};
+
+//TC : O(n)
+//SC : O(1)
