a14521d7b16ee0d38ee4f0ff71b497e3ada54d9b
dola <sanjogitabhagowati11@gmail.com>
2025-08-16 18:45:13 +0530

    Create 26. Best Time to Buy and Sell Stock.cpp

---
+++ ARRAYS1/26. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int n = prices.size();
+        int mini = prices[0];
+        int maxProfit = 0;
+
+        for (int i=0; i<n; i++)
+        {
+            int cost = prices[i]-mini;
+            maxProfit = max(cost ,maxProfit);
+            mini = min(prices[i],mini);
+        }
+
+        return maxProfit;
+    }
+};
+
+//TC : O(N)
+//SC : O(1)
