445a784e31b4c9b2dacbccedcb85d70c2be0e126
dola <sanjogitabhagowati11@gmail.com>
2025-08-21 21:10:37 +0530

    Update 4.noOfIslandsdfs.cpp

--- GRAPHS1/4.noOfIslandsdfs.cpp
+++ GRAPHS1/4.noOfIslandsdfs.cpp
@@ -36,3 +36,48 @@ public:
         return noOfIslands;
     }
 };
+
+
+//using visited array
+class Solution {
+public:
+    void dfs(int row, int col, vector<vector<int>>&visited, vector<vector<char>>&grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == '0' || visited[row][col] == 1) return;
+
+        visited[row][col] = 1;
+
+        dfs(row+1,col,visited,grid);
+        dfs(row,col+1,visited,grid);
+        dfs(row-1,col,visited,grid);
+        dfs(row,col-1,visited,grid);
+    }
+    int numIslands(vector<vector<char>>& grid) 
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        int noOfIslands =0;
+
+        vector<vector<int>>visited(m,vector<int>(n,0));
+
+        for (int i=0; i<m;i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if (grid[i][j] == '1' && visited[i][j] == 0)
+                {
+                    noOfIslands++;
+                    dfs(i,j,visited,grid);
+                }
+            }
+        }
+        return noOfIslands;
+    }
+};
+
+
+//TC : O(m × n)
+//SC : O(m × n)
