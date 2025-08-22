42816564c50fd867e3f937f1bcc4b654b8489245
Sanjogita Bhagowati <sanjogitabhagowati11@gmail.com>
2025-08-22 16:08:11 +0530

    Update 9.surroundRegions.cpp

--- GRAPHS1/9.surroundRegions.cpp
+++ GRAPHS1/9.surroundRegions.cpp
@@ -106,5 +106,61 @@ public:
     }
 };
 
+//bfs part 2
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) 
+    {
+        int m = board.size();
+        if (m == 0) return;
+        int n = board[0].size();
+        
+        queue<pair<int,int>> q;
+
+        for (int i=0; i<m; i++)
+        {
+            if (board[i][0] == 'O') q.push({i,0});
+            if (board[i][n-1] == 'O') q.push({i,n-1});
+        }
+
+        for (int j=0; j<n; j++)
+        {
+            if (board[0][j] == 'O') q.push({0,j});
+            if (board[m-1][j] == 'O') q.push({m-1,j});
+        }
+
+        vector<pair<int,int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.empty())
+        {
+            auto row = q.front().first;
+            auto col = q.front().second;
+            q.pop();
+            board[row][col] = 'S';
+
+            for (auto [dx,dy] : directions)
+            {
+                int nr = row + dx;
+                int nc = col + dy;
+
+                if (nr>=0 && nc >=0 && nr<m && nc<n && board[nr][nc] =='O')
+                {
+                    q.push({nr,nc});
+                    board[nr][nc] = 'S';
+                }
+            }
+        }
+
+        for (int i=0; i<m; i++)
+        {
+            for (int j=0; j<n; j++)
+            {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                if (board[i][j] == 'S') board[i][j] = 'O';
+            }
+        }
+    }
+};
+
 //TC : O(m*n)
 //SC : O(m*n)
